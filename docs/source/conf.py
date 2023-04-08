d31b9c6e00042f33621b18f31220f51f45f4461d
Frazer <39445054+frazzlesgrant1994@users.noreply.github.com>
2023-04-08 14:17:56 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Artemis OS'
+copyright = '2023, Artemis OS'
+author = 'Artemis OS'
 
 release = '0.1'
 version = '0.1.0'
